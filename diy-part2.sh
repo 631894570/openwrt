a39fb021be4fbf56406b78858e105aa731812bd5
631894570 <43724996+631894570@users.noreply.github.com>
2022-09-16 19:27:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.10/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
+
+#4.默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' openwrt/feeds/luci/collections/luci/Makefile
