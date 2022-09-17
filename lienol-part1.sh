94adf008ed6f25502dfa6eac2e6711347823408c
631894570 <43724996+631894570@users.noreply.github.com>
2022-09-17 18:36:31 +0800

    Update lienol-part1.sh

--- lienol-part1.sh
+++ lienol-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >>feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >>feeds.conf.default
