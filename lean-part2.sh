05cb466504c3206a1ae9b63a505cdefd3e564604
631894570 <43724996+631894570@users.noreply.github.com>
2022-09-20 12:14:08 +0800

    Update lean-part2.sh

--- lean-part2.sh
+++ lean-part2.sh
@@ -22,7 +22,7 @@ rm -rf openwrt/package/base-files/files/etc/banner
 cp banner openwrt/package/base-files/files/etc/banner
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.10/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
