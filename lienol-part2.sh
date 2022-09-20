d0ee04861635ff0120e36b920743e6d6dbe042d8
631894570 <43724996+631894570@users.noreply.github.com>
2022-09-20 12:14:23 +0800

    Update lienol-part2.sh

--- lienol-part2.sh
+++ lienol-part2.sh
@@ -24,7 +24,7 @@ cp banner openwrt/package/base-files/files/etc/banner
 sed -i 's/login/login -f root/g' openwrt/feeds/packages/utils/ttyd/files/ttyd.config
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.10/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
