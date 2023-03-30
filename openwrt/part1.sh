19c338c3f52368ab4a1af6de03536031111d5384
631894570 <43724996+631894570@users.noreply.github.com>
2023-03-30 17:45:03 +0800

    Update part1.sh

--- openwrt/part1.sh
+++ openwrt/part1.sh
@@ -1,1 +1,10 @@
+#添加kenzok8/small-package库
+sed -i '$a small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
+
+#添加iStoreOS
+svn co https://github.com/linkease/istore/trunk/luci package/istore
+svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+rm -rf package/istore/.svn
+sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci-app-store/Makefile
+sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci-app-store/luasrc/controller/store.lua
