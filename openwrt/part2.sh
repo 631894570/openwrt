2c29d9de9b3b1e70800fb193949f2da5789562eb
631894570 <43724996+631894570@users.noreply.github.com>
2023-03-30 15:27:28 +0800

    Update part2.sh

--- openwrt/part2.sh
+++ openwrt/part2.sh
@@ -37,3 +37,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/confi
 
 #4.默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-design/g' openwrt/feeds/luci/collections/luci-light/Makefile
+
+
+#修改默认语言
+sed -i 's/auto/zh_Hans/g' openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
