8e742e416aa13072af3bd6d1c11be26d3b9db7c5
631894570 <43724996+631894570@users.noreply.github.com>
2023-06-04 13:24:43 +0800

    Update part2.sh

--- lean/part2.sh
+++ lean/part2.sh
@@ -37,3 +37,7 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #4.默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' openwrt/feeds/luci/collections/luci/Makefile
+
+#alist编译需要
+rm -rf openwrt/feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x openwrt/feeds/packages/lang/golang
