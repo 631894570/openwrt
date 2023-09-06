812821f88da0e4c0ecb2f4ceeed32a0feb6f719b
631894570 <43724996+631894570@users.noreply.github.com>
2023-09-06 20:05:21 +0800

    Update part1.sh

--- panther-x2/part1.sh
+++ panther-x2/part1.sh
@@ -9,3 +9,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+#添加kenzok8/openwrt-packages
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
