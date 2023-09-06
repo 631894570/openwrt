8f85939ffe33c06ff3c7b5229ea8cef0b1bac817
631894570 <43724996+631894570@users.noreply.github.com>
2023-09-06 20:41:15 +0800

    Create part2.sh

---
+++ panther-x2/part2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+#修改TTYD自动登录
+sed -i 's/login/login -f root/g' openwrt/feeds/packages/utils/ttyd/files/ttyd.config
+
+#替换banner
+rm -rf openwrt/package/base-files/files/etc/banner
+curl -o openwrt/package/base-files/files/etc/banner https://raw.githubusercontent.com/631894570/openwrt/main/banner
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.10/g' openwrt/package/base-files/files/bin/config_generate
+
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#4.默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' openwrt/feeds/luci/collections/luci/Makefile
+
+#alist编译需要
+rm -rf openwrt/feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x openwrt/feeds/packages/lang/golang
+
+#配置向导
+git clone https://github.com/sirpdboy/luci-app-netwizard openwrt/feeds/luci/applications/
