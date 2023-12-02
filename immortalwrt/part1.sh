960eb03747b5b405a1b9f097ea660f59d4899d0e
yccz <43724996+631894570@users.noreply.github.com>
2023-12-02 23:56:24 +0800

    Update part1.sh

--- immortalwrt/part1.sh
+++ immortalwrt/part1.sh
@@ -18,14 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add kenzo feed source
-#sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-
-# 软件中心istore
-#svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
-svn co https://github.com/linkease/istore/trunk/luci package/istore
-svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-rm -rf package/istore/.svn
-sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci-app-store/Makefile
-sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci-app-store/luasrc/controller/store.lua
