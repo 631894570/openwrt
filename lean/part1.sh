844dd083a4933ed8ca4b4c8fdddc65d6977c08be
yccz <43724996+631894570@users.noreply.github.com>
2024-07-10 22:17:27 +0800

    Update part1.sh

--- lean/part1.sh
+++ lean/part1.sh
@@ -10,21 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# Add kenzo feed source
-sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-# 软件中心istore
-#svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
-svn co https://github.com/linkease/istore/trunk/luci package/istore
-svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-rm -rf package/istore/.svn
-sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci-app-store/Makefile
-sed -i 's/("iStore"), 31/("应用商店"), 61/g' package/istore/luci-app-store/luasrc/controller/store.lua
+sed -i '1i src-git yccz https://github.com/631894570/small-package' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
